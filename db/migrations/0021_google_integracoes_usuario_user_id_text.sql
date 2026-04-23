82679f838636be6b6232cbf540762c33079a1b8d
Gilberto Jacob <gilbertojacob@MacBook-Pro-de-Gilberto.local>
2026-04-23 16:57:26 -0300

    fix(db): corrige drop da fk na migration google

--- db/migrations/0021_google_integracoes_usuario_user_id_text.sql
+++ db/migrations/0021_google_integracoes_usuario_user_id_text.sql
@@ -1,4 +1,7 @@
 ALTER TABLE google_integracoes_usuario
+  DROP CONSTRAINT IF EXISTS google_integracoes_usuario_user_id_fkey;
+
+ALTER TABLE google_integracoes_usuario
   DROP CONSTRAINT IF EXISTS google_integracoes_usuario_user_id_users_id_fk;
 
 ALTER TABLE google_integracoes_usuario
